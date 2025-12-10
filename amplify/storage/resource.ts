da452270c81044a84dc2a3e7cf81667d38bffc3c
andywildeman <andy@wildeman.co.uk>
2025-12-10 18:30:52 +0000

    authentication applied

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,11 +3,17 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: "thDrive",
   access: (allow) => ({
-    "*": [
-      allow.authenticated.to(["read", "write"])  // allow ALL authenticated access
-    ],
     "public/*": [
       allow.guest.to(["read"]),
+      allow.authenticated.to(["read", "write"])
+    ],
+
+    "protected/*": [
+      allow.authenticated.to(["read", "write"])
+    ],
+
+    // 🔥 matches: c7534ee4-6115-48ac-a929-2e3f9ff9c770/<file>
+    "*/*": [
       allow.authenticated.to(["read", "write"])
     ]
   })
